da5fe449ec796ff044bb71fdde4f0ffd28e8ffb4
Dennis Bailey <bailey.dennis@gmail.com>
2016-04-04 13:33:22 -0600

    Added the ability to create new tasks and added strikethrough styling for completed tasks

--- src/js/app.js
+++ src/js/app.js
@@ -3,5 +3,13 @@
 var app = angular.module('allTheGreatTodos', []);
 
 app.controller('todoController', ['$scope', function($scope) {
+  
+  $scope.addTodo = function (todo) {
+    todo.done = false;
+    $scope.list.push(todo);
+    
+    $scope.todo = {};
+  }
+  
   $scope.list = [{ task: 'Get this app working', done: false}];
 }]);
